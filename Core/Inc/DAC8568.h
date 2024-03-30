d2077ffa6fac39467c27cb17d1593c9dec74a13b
ninedayhx <1170535490@qq.com>
2024-03-30 22:08:02 +0800

    feat:增加了DAC7568的部分驱动，并进行了测试

--- Core/Inc/DAC8568.h
+++ Core/Inc/DAC8568.h
@@ -15,7 +15,7 @@
 #include "main.h"
 #include "spi.h"
 
-extern int16_t ZeroCode[8];
+extern float ZeroCode[8];
 
 #define dac8568_range       10000.0	
 #define dac8568_shifting    5000.0	
@@ -23,10 +23,23 @@ extern int16_t ZeroCode[8];
 #define dac8568_sync_l  HAL_GPIO_WritePin(DAC_SYNC_GPIO_Port, DAC_SYNC_Pin, GPIO_PIN_RESET)
 #define dac8568_sync_h  HAL_GPIO_WritePin(DAC_SYNC_GPIO_Port, DAC_SYNC_Pin, GPIO_PIN_SET)
 
+#define SOFTWARE_RESET          0x07000000
+#define POWER_UP_ALL            0x040000FF
+#define POWER_DOWN_ALL_1k       0x040001FF
+#define POWER_DOWN_ALL_100k     0x040002FF
+#define POWER_DOWN_ALL_HIGHZ    0x040003FF
+#define EN_INTERNAL_REFERENCE   0x090A0000
+#define CLEAR_TO_ZEROSCALE      0x05000000
+#define CLEAR_TO_MIDSCALE       0x05000001
+#define CLEAR_TO_FULLSCALE      0x05000002
+
 void dac8568_init(void);
 void dac8568_write_data(uint32_t data);
-void dac8568_write_channel(uint8_t ch, uint16_t data);
-void dac8568_write_all_channel(uint16_t data);
+void dac8568_writeupdate_channel(uint8_t ch, uint16_t data);
+void dac8568_writeupdate_channelval(uint8_t ch, float val);
+void dac8568_writeupdate_all_channel(uint16_t data);
+void dac8568_writeupdate_all_channelval(float val);
+void dac8568_powerup_channel(uint8_t ch);
 void spi_read_write_byte(uint16_t txdata);
 
 #endif /* INC_DAC8568_H_ */
--- Core/Src/DAC8568.c
+++ Core/Src/DAC8568.c
@@ -10,23 +10,32 @@
  */
 #include "DAC8568.h"
 
-// 零偏校准
-int16_t ZeroCode[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+// 零偏校准 mv
+float ZeroCode[8] = {0.001f, 0, 0, 0.002f, 0, 0, 0, 0};
 
 void dac8568_init(void)
 {
 	__HAL_SPI_ENABLE(&hspi3);
 
 	spi_read_write_byte(0Xff);
-	// 复位指令
-	dac8568_write_data(0x07000000);	
-	//HAL_Delay(100);
-	// 全部通道上电
-	dac8568_write_data(0x040000FF);	
-	// 使用内部参考基准
-	dac8568_write_data(0x090A0000);	
-	// 初始化为0V
-	dac8568_write_all_channel(0.5*65535);
+	// 复位指令 清除寄存器中的值
+	dac8568_write_data(SOFTWARE_RESET);	
+	// HAL_Delay(100);
+	// 先写入寄存器，使其处于中值32767，如果不写入，初始值会变成0
+	// 那么通道上电时，会输出负电压
+//	dac8568_writeupdate_channelval(0,0.0);
+//	dac8568_writeupdate_channelval(1,0.0);
+//	dac8568_writeupdate_channelval(2,0.0);
+//	dac8568_writeupdate_channelval(3,0.0);
+//	// 使能对应通道上电
+//	dac8568_powerup_channel(0);
+//	dac8568_powerup_channel(1);
+//	dac8568_powerup_channel(2);
+//	dac8568_powerup_channel(3);
+//
+//	// 使用内部参考基准
+//	dac8568_write_data(EN_INTERNAL_REFERENCE);
+
 }
 
 /**
@@ -56,12 +65,12 @@ void dac8568_write_data(uint32_t data)
 }
 
 /**
- * @brief 更新指定通道的数据
+ * @brief 写指定通道的寄存器，并更新
  * 
  * @param ch 通道数 0-7 对应 A-G
  * @param data 0 ~ 65535
  */
-void dac8568_write_channel(uint8_t ch, uint16_t data)
+void dac8568_writeupdate_channel(uint8_t ch, uint16_t data)
 {
 	uint32_t txdata = 0x03000000;
 	txdata |= (uint32_t)(ch&0x0F)<<20;
@@ -71,16 +80,61 @@ void dac8568_write_channel(uint8_t ch, uint16_t data)
 }
 
 /**
- * @brief 直接配置所有通道
+ * @brief 写指定通道的电压，并更新，在函数中进行了零偏矫正
+ * 
+ * @param ch 通道数 0-7 对应 A-G
+ * @param val 0-2.0V
+ */
+void dac8568_writeupdate_channelval(uint8_t ch, float val)
+{
+	if(val>=2.0) val = 2.0;
+	if(val<=0.0) val = 0.0;
+	uint16_t data = 32767 + (uint16_t)((val-ZeroCode[ch])/5.0f*32767.0f);
+	uint32_t txdata = 0x03000000;
+	txdata |= (uint32_t)(ch&0x0F)<<20;
+	txdata |= (uint32_t)data<<4;
+	
+	dac8568_write_data(txdata);
+}
+
+/**
+ * @brief 写所有通道寄存器，并更新
  * 
  * @param data 
  */
-void dac8568_write_all_channel(uint16_t data)
+void dac8568_writeupdate_all_channel(uint16_t data)
 {
 	uint32_t txdata = 0x03F00000;
 	
 	txdata |= (uint32_t)data<<4;
 	
+	dac8568_write_data(txdata);
+}
+
+/**
+ * @brief 写所有通道电压值，并进行更新，注意此函数无法对零偏进行矫正
+ * 
+ * @param val
+ */
+void dac8568_writeupdate_all_channelval(float val)
+{
+	if(val>=2.0) val = 2.0;
+	if(val<=0.0) val = 0.0;
+	uint16_t data = 32767 + (uint16_t)(val/5.0f*32767.0f);
+
+	uint32_t txdata = 0x03F00000;
+	
+	txdata |= (uint32_t)data<<4;
+	
+	dac8568_write_data(txdata);
+}
+
+void dac8568_powerup_channel(uint8_t ch){
+	static uint8_t channel = 0x00;
+	channel |= 1<<(ch-1);
+	uint32_t txdata = 0x04000000;
+	txdata |= (uint32_t)channel;
+
 	dac8568_write_data(txdata);
 }
 
